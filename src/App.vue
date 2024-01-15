f392c112daf54a28830eabec995582970f03f17f
DorinVieru <dorino.vieru@gmail.com>
2024-01-15 10:27:40 +0100

    [UP] Complete MILESTONE 0

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
-<template lang="">
-  <div>
-    
-  </div>
-</template>
+<!-- SEZIONE JS SCRIPT -->
 <script>
+
+// IMPORTO LE COMPONTENTI
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+import { store } from './store.js';
+
 export default {
-  
+  components: {
+    AppHeader,
+    AppMain,
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    
+  },
+  created() {
+    
+  },
 }
 </script>
-<style lang="">
-  
+
+<!-- SEZIONE HTML -->
+<template lang="">
+  <div>
+      <!-- HEADER -->
+      <AppHeader />
+      
+      <!-- MAIN -->
+      <AppMain />
+  </div>
+</template>
+
+<!-- SEZIONE STYLE -->
+<style lang="scss">
+@use './styles/partials/variables' as *;
+@use './styles/generals.scss' as *;
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<!-- SEZIONE JS SCRIPT -->
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<!-- SEZIONE HTML -->
+<template lang="">
+  <div>
+      
+  </div>
+</template>
+
+<!-- SEZIONE STYLE -->
+<style lang="scss">
+@use '../styles/partials/variables' as *;
+@use '../styles/generals.scss' as *;
+</style>
